655705dc434d6d696ade4f9ea4d011473f6c14c3
Casey Mulcahy <caseymulcahy@proton.me>
2024-02-11 13:53:23 -0500

    Simple working component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Cell from '$lib/Cell.svelte';
+    let values = [1, 2, 3];
+</script>
+
+<h1>Shellsheet</h1>
+{#each values as value}
+<Cell {value}/>
+{/each}
