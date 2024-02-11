51d5a3f3acb5a86ae06e0383f8bf23ba92aa0c55
Casey Mulcahy <caseymulcahy@proton.me>
2024-02-11 16:04:25 -0500

    Nicer styles

--- src/lib/Cell.svelte
+++ src/lib/Cell.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
+    export let id: string;
     export let value: any;
 </script>
 
-<p>Value: {value}</p>
+<style>
+    div.cell {
+        border: 1px solid gray;
+        margin: 0.5rem;
+        width: fit-content;
+    }
+    div.content {
+        padding: 0.5rem;
+    }
+    div.header {
+        margin: 0;
+        padding: 0.5rem;
+        background-color: lightblue;
+    }
+    div.header input {
+        background-color: inherit;
+        border: 0;
+        font-size: large;
+        font-weight: bold;
+    }
+    div.type {
+        font-style: italic;
+        font-size: small;
+    }
+</style>
+
+<div class="cell">
+    <div class="header">
+        <input id="id" type="text" size="8" bind:value={id}/>
+        <div class="type">{typeof value}</div>
+    </div>
+    <div class="content">
+        {value}
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,22 @@
     let values = [1, 2, 3];
 </script>
 
+<style>
+    h1 {
+        background-color: lightsteelblue;
+        margin: 0;
+        padding: 0.5rem;
+        text-align: center;
+        border-bottom: 1px solid steelblue;
+    }
+    div.workspace {
+        padding: 0.5rem;
+    }
+</style>
+
 <h1>Shellsheet</h1>
-{#each values as value}
-<Cell {value}/>
-{/each}
+<div class="workspace">
+    {#each values as value, i}
+    <Cell id={i.toString()} {value}/>
+    {/each}
+</div>
