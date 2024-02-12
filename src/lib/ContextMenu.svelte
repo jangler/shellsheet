913136eb17fe677c75a6045bc9147af11b8b1c1f
Casey Mulcahy <caseymulcahy@proton.me>
2024-02-12 13:58:20 -0500

    Move context menu state to component

--- src/lib/ContextMenu.svelte
+++ src/lib/ContextMenu.svelte
@@ -1,40 +1,57 @@
-<script lang="ts">
-    export let x: number;
-    export let y: number;
-    export let items: { text: string, callback: () => any }[];
+<script lang="ts" context="module">
+    import { writable } from 'svelte/store';
 
-    let element: HTMLMenuElement;
-    
+	type Item = {
+		text: string;
+		callback: () => void;
+	};
+
+	let visible = writable(false);
+	let x = writable(0);
+	let y = writable(0);
+	let items = writable([] as Item[]);
+
+	export function showContextMenu(x_: number, y_: number, items_: Item[]) {
+		x.set(x_);
+		y.set(y_);
+		items.set(items_);
+		visible.set(true);
+	}
+
     // TODO: Add keyboard accelerators.
 </script>
 
-<style>
-    menu {
-        position: absolute;
-        background-color: white;
-        border: 1px solid black;
-        border-radius: 2px;
-        padding: 0;
-        list-style: none;
-    }
-    menu li button {
-        background-color: inherit;
-        border: 0;
-        border-radius: 0;
-        text-align: left;
-        padding: 0.4rem;
-        width: 100%;
-    }
-    menu li button:hover {
-        background-color: hsl(200, 80%, 95%);
-    }
-    menu li button:active {
-        background-color: hsl(200, 90%, 90%);
-    }
-</style>
+<svelte:document on:mouseup={() => visible.set(false)} />
 
-<menu bind:this={element} style:left={`${x}px`} style:top={`${y - 16}px`}>
-    {#each items as item}
-    <li><button on:mouseup={item.callback}>{item.text}</button></li>
-    {/each}
-</menu>
+{#if $visible}
+	<menu style:left={`${$x}px`} style:top={`${$y - 16}px`}>
+		{#each $items as item}
+			<li><button on:mouseup={item.callback}>{item.text}</button></li>
+		{/each}
+	</menu>
+{/if}
+
+<style>
+	menu {
+		position: absolute;
+		background-color: white;
+		border: 1px solid black;
+		border-radius: 2px;
+		padding: 0;
+		list-style: none;
+	}
+	menu li button {
+		background-color: inherit;
+		border: 0;
+		border-radius: 0;
+		text-align: left;
+		padding: 0.4rem;
+		width: 100%;
+	}
+	menu li button:hover {
+		background-color: hsl(200, 80%, 95%);
+	}
+	menu li button:active {
+		background-color: hsl(200, 90%, 90%);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,55 @@
 <script lang="ts">
-    import Cell, { cells, firstFreeName } from '$lib/Cell.svelte';
-	import ContextMenu from '$lib/ContextMenu.svelte';
-    import Message from '$lib/Message.svelte';
-    import { messages } from '$lib/messages';
+	import Cell, { cells, firstFreeName } from '$lib/Cell.svelte';
+	import ContextMenu, { showContextMenu } from '$lib/ContextMenu.svelte';
+	import Message from '$lib/Message.svelte';
+	import { messages } from '$lib/messages';
 
-    let menuVisible = false;
-    let menuX: number;
-    let menuY: number;
+	function showWorkspaceContextMenu(e: MouseEvent) {
+		e.preventDefault();
+		showContextMenu(e.clientX, e.clientY, [
+			{ text: 'Add cell', callback: addCell },
+			{ text: 'Clear workspace', callback: cells.clear }
+		]);
+	}
 
-    function showContextMenu(e: MouseEvent) {
-        e.preventDefault();
-        menuX = e.clientX;
-        menuY = e.clientY;
-        menuVisible = true;
-    }
-
-    function addCell() {
-        cells.push({
-            name: firstFreeName(),
-            value: 0,
-            editable: true,
-        });
-    }
-
-    let menuItems = [
-        { text: "Add cell", callback: addCell },
-        { text: "Clear workspace", callback: cells.clear },
-    ];
+	function addCell() {
+		cells.push({
+			name: firstFreeName(),
+			value: 0,
+			editable: true
+		});
+	}
 </script>
-
-<style>
-    h1 {
-        background-color: lightsteelblue;
-        margin: 0;
-        padding: 0.5rem;
-        text-align: center;
-        border-bottom: 1px solid steelblue;
-    }
-    div.workspace {
-        padding: 0.5rem;
-        border-bottom: 1px solid steelblue;
-    }
-    div.messages {
-        position: fixed;
-        bottom: 0;
-        right: 0;
-    }
-</style>
-
-<svelte:document on:mouseup={() => menuVisible = false}/>
 
 <h1>Shellsheet</h1>
 <!-- FIXME: What ARIA role to use here? -->
-<div class="workspace" on:contextmenu={showContextMenu}>
-    {#each $cells as cell}
-    <Cell {...cell}/>
-    {/each}
+<div class="workspace" on:contextmenu={showWorkspaceContextMenu}>
+	{#each $cells as cell}
+		<Cell {...cell} />
+	{/each}
 </div>
 <div class="messages">
-    {#each $messages as message}
-    <Message {...message}/>
-    {/each}
+	{#each $messages as message}
+		<Message {...message} />
+	{/each}
 </div>
-{#if menuVisible}
-<ContextMenu x={menuX} y={menuY} items={menuItems}/>
-{/if}
+<ContextMenu />
+
+<style>
+	h1 {
+		background-color: lightsteelblue;
+		margin: 0;
+		padding: 0.5rem;
+		text-align: center;
+		border-bottom: 1px solid steelblue;
+	}
+	div.workspace {
+		padding: 0.5rem;
+		border-bottom: 1px solid steelblue;
+	}
+	div.messages {
+		position: fixed;
+		bottom: 0;
+		right: 0;
+	}
+</style>
